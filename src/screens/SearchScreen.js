1c4a26108c00482cb2babc40c60fac16ead91cd7
MounikaRangisetty <83297162+MounikaRangisetty@users.noreply.github.com>
2024-09-20 11:26:43 +0530

    Update SearchScreen.js

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -92,7 +92,7 @@ export default function SearchScreen() {
     const fetchData = async () => {
       try {
         const { data } = await axios.get(
-          `http://54.92.143.126:4000/api/products/search?page=${page}&query=${query}&category=${category}&price=${price}&rating=${rating}&order=${order}`
+          `http://34.235.146.105:4000/api/products/search?page=${page}&query=${query}&category=${category}&price=${price}&rating=${rating}&order=${order}`
         );
         dispatch({ type: 'FETCH_SUCCESS', payload: data });
       } catch (err) {
@@ -109,7 +109,7 @@ export default function SearchScreen() {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const { data } = await axios.get(`http://54.92.143.126:4000/api/products/categories`);
+        const { data } = await axios.get(`http://34.235.146.105:4000/api/products/categories`);
         setCategories(data);
       } catch (err) {
         toast.error(getError(err));
